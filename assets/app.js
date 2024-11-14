fa2dd6f6c90594c2a91380f0074b6dcef2fa22e5
EmmaVgn <emma.vigneron@outlook.fr>
2024-11-14 17:39:30 +0100

    ajout des mentiosn legales

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,8 @@ import './styles/app.css';
 import './styles/carousel.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
-
+import './js/cookieconsent.min.js';
+import './styles/cookieconsent.min.css';
 
 
 /*
@@ -15,3 +16,42 @@ import 'bootstrap';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (
+        value || ""
+    ) + expires + "; path=/; SameSite=None; Secure";
+}
+window.addEventListener('load', function () {
+    console.log('Initialisation de CookieConsent');
+    if (window.cookieconsent) {
+        window.cookieconsent.initialise({
+            "palette": {
+                "popup": {
+                    "background": "#000"
+                },
+                "button": {
+                    "background": "#f1d600"
+                }
+            },
+            "theme": "classic",
+            "position": "bottom",
+            "content": {
+                "message": "Nous utilisons des cookies pour améliorer votre expérience sur notre site.",
+                "dismiss": "Accepter",
+                "link": "En savoir plus",
+                "href": "/politique-de-confidentialite"
+            },
+            onStatusChange: function (status) {
+                setCookie('cookieconsent_status', status, 365);
+            }
+        });
+    } else {
+        console.error('CookieConsent library not loaded.');
+    }
+});
