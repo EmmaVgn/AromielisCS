c888f418bdeb337ae613bb8d35eb000348cb026c
EmmaVgn <emma.vigneron@outlook.fr>
2025-02-03 15:06:12 +0100

    supp cookies

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,6 @@ import './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './styles/carousel.css';
-import './js/cookieconsent.min.js';
-import './styles/cookieconsent.min.css';
 
 import Filter from './js/Filter.js';
 new Filter(document.querySelector('.js-filter'))
@@ -22,47 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
             behavior: "smooth", // Défilement fluide
         });
     });
-});
-
-//Cookie
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (
-        value || ""
-    ) + expires + "; path=/; SameSite=None; Secure";
-}
-window.addEventListener('load', function () {
-    console.log('Initialisation de CookieConsent');
-    if (window.cookieconsent) {
-        window.cookieconsent.initialise({
-            "palette": {
-                "popup": {
-                    "background": "#000"
-                },
-                "button": {
-                    "background": "#f1d600"
-                }
-            },
-            "theme": "classic",
-            "position": "bottom",
-            "content": {
-                "message": "Nous utilisons des cookies pour améliorer votre expérience sur notre site.",
-                "dismiss": "Accepter",
-                "link": "En savoir plus",
-                "href": "/politique-de-confidentialite"
-            },
-            onStatusChange: function (status) {
-                setCookie('cookieconsent_status', status, 365);
-            }
-        });
-    } else {
-        console.error('CookieConsent library not loaded.');
-    }
 });
 
 //graphique stats
